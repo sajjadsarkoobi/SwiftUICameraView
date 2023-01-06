a90bb20485efcd8414dfd4feb5cf2866bc80ffb0
Sajjad Sarkoobi <sajjadsarkoobi@gmail.com>
2023-01-06 11:53:59 +0300

    Initial Commit

---
+++ SwiftUICameraView/SwiftUICameraViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICameraViewApp.swift
+//  SwiftUICameraView
+//
+//  Created by Sajjad Sarkoobi on 6.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICameraViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
